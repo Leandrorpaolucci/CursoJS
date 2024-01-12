a4607f6beeda1d0093e25bba5352394e4d5c40fa
Leandroydc <leandroydc2@gmail.com>
2024-01-12 18:11:52 -0300

    js

---
+++ aula13/aula13.js
@@ -0,0 +1,16 @@
+//               012345679
+let umaString = 'O rato roeu a roupa do reu de roma.';
+
+//console.log(umaString.concat(' ', 'em', ' ' , 'um', ' Lindo dia'));
+//console.log(umaString + ' em um lindo dia.');
+//console.log(`${umaString} em um lindo dia`)
+
+//console.log(umaString.charAt(2))
+//console.log(umaString.indexOf('o', 3));
+//console.log(umaString.lastIndexOf('m', 3))
+//console.log(umaString.match(/[a-z]/));
+//console.log(umaString.replace(/r/g, '#'));
+//console.log(umaString.length);
+//console.log(umaString.slice(-5, umaString.length -1));
+//console.log(umaString.toUpperCase());
+//console.log(umaString.toLowerCase());
---
+++ aula14/index.js
@@ -0,0 +1,5 @@
+let num1 = 1; //Number
+let num2 = 2.5;
+
+console.log(num1.toString() + num2);
+console.log(typeof num1)
