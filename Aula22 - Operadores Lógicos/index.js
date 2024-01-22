2bbec68ed3ded4c1d6097dbb6450f820a31854a7
Leandro Ribeiro Casa <leandroydc2@gmail.com>
2024-01-21 23:58:59 -0300

    Operadores Comparação e Lógicos

---
+++ Aula22 - Operadores Lógicos/index.js
@@ -0,0 +1,10 @@
+/*
+Operadores Lógicos
+
+&& -> AND -> E
+|| -> OR -> OU 
+!  -> NOT -> NÃO
+
+*/
+
+//console.log(true);
---
+++ aula21 - Operadores de Comparação/index.js
@@ -0,0 +1,22 @@
+/*
+
+                        Operadores de comparação
+>   maior que
+>=  maior que ou igual a
+<   menor que
+<=  menor que ou igual a
+=== igualdade estrita (valor e tipo)
+!== diferente estrito (valor e tipo)
+
+*/
+
+const comparacaoMaior = 10 > 5; // checa o da direita com o da esquerda
+const comparacaoMaiorOu = 10 >= 10;
+const num1 = 10;   //Number
+const num2 = '10'; //String
+const comparacaoMenor = num1 < num2
+const comparacaoMenorOu = num1 <= num2;
+const igualestrigo = num1 === num2
+const diferenteEstrito = num1 !== num2
+
+console.log(diferenteEstrito); // boolean 
