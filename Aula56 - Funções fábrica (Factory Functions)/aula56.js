773eae6f71256ffb3561ee13049158a49b4b64d6
Leandroydc <leandroydc2@gmail.com>
2024-02-15 18:00:24 -0300

    Projeto Calculadora com Factory Function

--- Aula56 - Funções fábrica (Factory Functions)/aula56.js
+++ Aula56 - Funções fábrica (Factory Functions)/aula56.js
@@ -5,33 +5,44 @@ function criaPessoa(nome, sobrenome, altura, peso) {
   return {
     nome,
     sobrenome,
+
     //Getter
     get nomeCompleto() {
-      return `${this.nome} ${this.sobrenome}`;
+      return `${this.nome} ${sobrenome}`;
     },
-
     //Setter
     set nomeCompleto(valor) {
       valor = valor.split(" ");
-      this.nome = valor.shift();
-      this.sobrenome = valor.join(" ");
       console.log(valor);
+      this.nome = valor.shift();
+      console.log(valor);
+      this.sobrenome = valor.join();
     },
-
+    // #2
     fala: function (assunto) {
-      return `${this.nome} está ${assunto}.`;
+      return `${
+        this.nome + " " + this.sobrenome
+      } está falando sobre ${assunto}.`;
     },
     altura: altura,
     peso: peso,
-
-    //Getter
+    // getter, faz o método fingir ser um atributo
     get imc() {
       const indice = this.peso / this.altura ** 2;
-      return indice.toFixed(2);
+      return `${this.nome}, seu IMC: ${indice.toFixed(2)}`;
     },
   };
 }
 
-const p1 = criaPessoa("Leandro", "Ribeiro", 1.8, 83);
-p1.nomeCompleto = "Camila Ribeiro Paolucci";
+const p1 = criaPessoa("Leandro", "Ribeiro", 1.83, 84);
+// #1 Executando a função
+console.log(p1);
+// #2 - Executando o método de fala:
+console.log(p1.fala("video games"));
+console.log(p1.nome);
+console.log(p1.imc);
 console.log(p1.nomeCompleto);
+p1.nomeCompleto = "Camila Oliveira";
+console.log(p1.nomeCompleto);
+console.log(p1.sobrenome);
+
