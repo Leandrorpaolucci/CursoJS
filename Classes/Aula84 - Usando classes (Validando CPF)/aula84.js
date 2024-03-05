195cda7d903d338c6e9674ce3c4ae0fe2d8aa8ce
Leandroydc <leandroydc2@gmail.com>
2024-03-05 17:12:53 -0300

    aprendizados com classes

--- Classes/Aula84 - Usando classes (Validando CPF)/aula84.js
+++ Classes/Aula84 - Usando classes (Validando CPF)/aula84.js
@@ -10,24 +10,51 @@ class ValidaCPF {
     });
   }
 
-  éSequência() {
-    return this.cpfLimpo.charAt(0).repeat(11) === this.cpfLimpo;
+  éSequencia() {
+    return (
+      this.cpfLimpo.charAt(0).repeat(this.cpfLimpo.length) === this.cpfLimpo
+    );
   }
 
-  geraNovoCPF() {
-    
+  geraNovoCpf() {
+    const cpfSemDigitos = this.cpfLimpo.slice(0, -2);
+    const digito1 = ValidaCPF.geraDigito(cpfSemDigitos);
+    const digito2 = ValidaCPF.geraDigito(cpfSemDigitos + digito1);
+    this.geraNovoCpf = cpfSemDigitos + digito1 + digito2;
+  }
+
+  static geraDigito(cpfSemDigitos) {
+    let total = 0;
+    let reverso = cpfSemDigitos.length + 1;
+
+    for (let stringNumerica of cpfSemDigitos) {
+      //console.log(stringNumerica, reverso);
+      total += reverso * Number(stringNumerica);
+      reverso--;
+    }
+
+    const digito = 11 - (total % 11);
+    return digito <= 9 ? String(digito) : "0";
+    console.log(total);
   }
 
   valida() {
     if (!this.cpfLimpo) return false;
     if (typeof this.cpfLimpo !== "string") return false;
     if (this.cpfLimpo.length !== 11) return false;
-    if (this.éSequência()) return false;
-    if (!this.geraNovoCPF()) return false;
+    if (this.éSequencia()) return false;
+    this.geraNovoCpf();
+    return this.geraNovoCpf === this.cpfLimpo;
+
     return "CHEGUEI AQUI";
   }
 }
 
-let validacpf = new ValidaCPF("705.484.450-52");
-validacpf = new ValidaCPF("111.111.111-11");
-console.log(validacpf.valida());
+let validacpf = new ValidaCPF("070.987.720-03");
+//validacpf = new ValidaCPF("111.111.111-11");
+
+if (validacpf.valida()) {
+  console.log("CPF válido");
+} else {
+  console.log("CPF inválido.");
+}
